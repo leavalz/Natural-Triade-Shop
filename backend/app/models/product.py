4bfd5af9095791bff79b7ec7bbb2c52e1aa4ad0d
leavalz <lvalenzuelapallerano@gmail.com>
2025-10-20 17:06:12 -0300

    Added image/s to the product model

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, Enum
+from sqlalchemy import Column, Integer, String, Float, Boolean, Enum, Text
 import enum
 from app.core.database import Base
 
@@ -12,8 +12,10 @@ class Product(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(300), nullable=False)
-    description = Column(String(600), nullable=False)
+    description = Column(Text, nullable=False)
     price = Column(Float, nullable=False)
     stock = Column(Integer, default=0)
     is_active = Column(Boolean, default=True)
-    category = Column(Enum(ProductCategory), nullable=True)
+    category = Column(Enum(ProductCategory), nullable=True, index=True)
+    image_url = Column(String(500), nullable=True)
+    image_urls = Column(Text, nullable=True)
