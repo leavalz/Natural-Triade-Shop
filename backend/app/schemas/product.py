9a22685fb8bb9d048eaaeb4a844ada0519e6a363
leavalz <lvalenzuelapallerano@gmail.com>
2025-10-20 17:28:48 -0300

    Added image/s to the product schema, updated some fields and also created the ProductUpdate class

--- backend/app/schemas/product.py
+++ backend/app/schemas/product.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from enum import Enum
+from typing import Optional, List
 
 class ProductCategoryEnum(str, Enum):
     FACIAL = "facial"
@@ -7,14 +8,25 @@ class ProductCategoryEnum(str, Enum):
     CABELLO = "cabello"
 
 class ProductBase(BaseModel):
-    name: str
+    name: str = Field(..., min_length=1, max_length=200)
     description: str
-    price: float
-    stock: int = 0
-    category: ProductCategoryEnum | None = None 
+    price: float  = Field(..., gt=0)
+    stock: int = Field(default=0, ge=0)
+    category: Optional[ProductCategoryEnum] = None 
+    image_url: Optional[str] = None
+    image_urls: Optional[str] = None
 
 class ProductCreate(ProductBase):
     pass
+
+class ProductUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = Field(None, gt=0)
+    stock: Optional[int] = Field(None, ge=0)
+    category: Optional[ProductCategoryEnum] = None 
+    image_url: Optional[str] = None
+    is_active: Optional[bool] = None
 
 class ProductResponse(ProductBase):
     id: int 
